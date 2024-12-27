bca85b333b1b527da1a40e94f5643891d4217e3d
Bouisr <r.bouis14@gmail.com>
2024-12-27 19:52:37 +0100

    Début Cart + Order

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule, ProductModule],
+  imports: [UserModule, DatabaseModule, ProductModule, OrderModule, CartModule],
   controllers: [],
   providers: [],
 })
