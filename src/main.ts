4a223849df45eb26488a87ea12946d9042bd578d
Rhayonne Lopes <rhayonne@gmail.com>
2025-02-11 16:42:01 +0100

    Start project module, changed. The main.ts has changed to show if the webserver works or not, if works, a msg wil appear, the same if doesn't work. The error msg will show in the terminal log'

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    const port = process.env.PORT || 8000;
+    await app.listen(port);
+
+    app.getHttpServer().once('Listening', () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log('Error starting the application', error);
+  }
 }
+
 bootstrap();
