eff5a0b983230f6a351025535d69bdebf298b455
Rhayonne Lopes <rhayonne@gmail.com>
2025-04-04 16:23:52 +0200

    Update documentation

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,17 +1,44 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import { Document } from 'mongoose';
 
 @Schema()
 export class User extends Document {
+  @ApiProperty({
+    description: "Nom de famille de l'utilisateur",
+    example: 'Doe',
+    required: true,
+    type: String,
+  })
   @Prop({ required: true })
   lastname: string;
 
+  @ApiProperty({
+    description: "Prénom de l'utilisateur",
+    example: 'John',
+    required: true,
+    type: String,
+  })
   @Prop({ required: true })
   firstname: string;
 
+  @ApiProperty({
+    description: "Adresse e-mail de l'utilisateur",
+    example: 'john.doe@example.com',
+    required: true,
+    type: String,
+    format: 'email', // Indique le format attendu
+  })
   @Prop({ required: true })
   email: string;
 
+  @ApiProperty({
+    description:
+      "Mot de passe de l'utilisateur (ne sera généralement pas affiché dans les réponses)",
+    example: 'motdepasseSecurise',
+    required: true,
+    type: String,
+  })
   @Prop({ required: true })
   password: string;
 }
